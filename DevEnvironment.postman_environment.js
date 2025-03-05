8f5eb08df18aa9b5f04fc922519d7f0500fc6022
anikush-yes <anikush@hotmail.com>
2025-03-05 13:49:45 +0200

    deleted Dev_Environment.postman added DevEnvironment.postman_environment.js
    Signed-off-by: anikush-yes <anikush@hotmail.com>

--- DevEnvironment.postman_environment.js
+++ DevEnvironment.postman_environment.js
@@ -10,6 +10,6 @@
 		}
 	],
 	"_postman_variable_scope": "environment",
-	"_postman_exported_at": "2025-03-05T08:22:17.150Z",
+	"_postman_exported_at": "2025-03-05T11:48:49.013Z",
 	"_postman_exported_using": "Postman/11.34.5"
 }
