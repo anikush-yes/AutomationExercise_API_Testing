b523e29aa68e96059e045d18b267caef4b1396c9
anikush-yes <anikush@hotmail.com>
2025-03-05 08:52:50 +0200

    added .gitignore and DevEnvironment.postman_environment.js to .
    Signed-off-by: anikush-yes <anikush@hotmail.com>

---
+++ DevEnvironment.postman_environment.js
@@ -0,0 +1,15 @@
+{
+	"id": "2808c7f4-bf60-4f34-a596-40858eb3aac9",
+	"name": "New Environment",
+	"values": [
+		{
+			"key": "base_url",
+			"value": "https://automationexercise.com/api/",
+			"type": "default",
+			"enabled": true
+		}
+	],
+	"_postman_variable_scope": "environment",
+	"_postman_exported_at": "2025-03-05T06:41:31.369Z",
+	"_postman_exported_using": "Postman/11.34.5"
+}
